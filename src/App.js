4f51420289329da62c3f02089b33a53772932a72
Bilgehan Aygün <Bilgehanaygn@gmail.com>
2022-03-09 01:59:11 +0300

    deploy breakout

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ function App() {
       <MyAppBar />
       <Router basename='https://bilgehanaygn.github.io/website/'>
         <Routes>
-          <Route path='https://bilgehanaygn.github.io/website/' element={<MainPage/>}/>
-          <Route path='https://bilgehanaygn.github.io/website/articles/:id' element={<PostDetails />} />
-          <Route path='https://bilgehanaygn.github.io/website/about' element={<About />} />
+          <Route path='/' element={<MainPage/>}/>
+          <Route path='/articles/:id' element={<PostDetails />} />
+          <Route path='/about' element={<About />} />
           <Route path='https://bilgehanaygn.github.io/website/' element={<Navigate to="/articles" />} />
         </Routes>
       </Router>
